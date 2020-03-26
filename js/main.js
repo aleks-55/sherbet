e27e7872aed15b5c4d38dcaa4f2499f296eed15e
aleks-55 <aleks.rasskazov.88@yandex.ru>
2020-03-27 02:11:24 +0600

    removed STEP 2 (get query_hash)

--- js/main.js
+++ js/main.js
@@ -67,12 +67,12 @@ function init() {
 function startSherbet(urlUserPage) {
     // id канала из страницы пользователя
     let chId = "";
-    // накоком посте закончился посл. из сделанных запроров request_big_count_post
+    // на каком посте закончился посл. из сделанных запроров request_big_count_post
     let after = '';
     // по сколько постов запрашивать? в request_big_count_post
     let countPost = 100;
     // берется из js-файла
-    let query_hash = '';
+    let query_hash = 'e769aa130647d2354c40ea6a439bfc08';
 
     // STEP 1 -------------------------------------
     // получим страницу пользователя (канала)
@@ -91,14 +91,15 @@ function startSherbet(urlUserPage) {
         // преобразуем ответ в DOM-объект
         let doc  = new DOMParser().parseFromString(xhr.responseText, 'text/html');
         // и получим url на js-файл в котором лежит query_hash
-        let script = doc.querySelectorAll("script[src]"); 
-        let url_js = "";
-        for (let i = 0; i < script.length; i++) {
-            if (script[i].src.match("ProfilePageContainer.js")) {
-                url_js = "https://www.instagram.com" + script[i].outerHTML.split('src="')[1].split('"')[0];
-                break;
-            }
-        }
+        let script;
+        // script = doc.querySelectorAll("script[src]"); 
+        // let url_js = "";
+        // for (let i = 0; i < script.length; i++) {
+        //     if (script[i].src.match("ProfilePageContainer.js")) {
+        //         url_js = "https://www.instagram.com" + script[i].outerHTML.split('src="')[1].split('"')[0];
+        //         break;
+        //     }
+        // }
 
         // теперь извлечем id канала из страницы пользователя
         script = doc.querySelectorAll("script"); 
@@ -119,7 +120,9 @@ function startSherbet(urlUserPage) {
                                 getCountPubAll(xhr.responseText) +
                                 " публикаций.<br />Ищем все " + getMonthRusPrilog(mymonth) + " публикации.";
 
-        step2(url_js);
+        // step2(url_js);
+        // step 3
+        request_big_count_post();
     }
 
     xhr.onerror = function(){
@@ -128,34 +131,34 @@ function startSherbet(urlUserPage) {
 
     // STEP 2 -------------------------------------
     // загрузим js-файл и получим query_hash
-    function step2(urlJs) {
-        let xhr_query_hash = new XMLHttpRequest();
-        xhr_query_hash.open('GET', urlJs);
-        xhr_query_hash.send();
+    // function step2(urlJs) {
+    //     let xhr_query_hash = new XMLHttpRequest();
+    //     xhr_query_hash.open('GET', urlJs);
+    //     xhr_query_hash.send();
 
-        xhr_query_hash.onload = function() {
-            if (xhr_query_hash.status != 200) {
-                // обработать ошибку
-                alert("Ошибка при загрузке дополнительного файла. Ответ сервера: " + xhr_query_hash.status);
-                document.getElementById("refresh_btn").classList.add('visible_on');
-                return;
-            }
-            if (t = xhr_query_hash.responseText.match(/s.pagination},queryId:"([A-Za-z0-9_-]+)"/)) {
-                query_hash = t[1];
-            } else {
-                alert('Ошибка.\nВ дополнительном файле не найден query_hash (queryId)');
-                document.getElementById("refresh_btn").classList.add('visible_on');
-                return;
-            }
-            //return query_hash;
+    //     xhr_query_hash.onload = function() {
+    //         if (xhr_query_hash.status != 200) {
+    //             // обработать ошибку
+    //             alert("Ошибка при загрузке дополнительного файла. Ответ сервера: " + xhr_query_hash.status);
+    //             document.getElementById("refresh_btn").classList.add('visible_on');
+    //             return;
+    //         }
+    //         if (t = xhr_query_hash.responseText.match(/s.pagination},queryId:"([A-Za-z0-9_-]+)"/)) {
+    //             query_hash = t[1];
+    //         } else {
+    //             alert('Ошибка.\nВ дополнительном файле не найден query_hash (queryId)');
+    //             document.getElementById("refresh_btn").classList.add('visible_on');
+    //             return;
+    //         }
+    //         //return query_hash;
 
-            request_big_count_post();
-        }
+    //         request_big_count_post();
+    //     }
 
-        xhr_query_hash.onerror = function(){
-            alert('Ошибка соединения.\nВозникла при выполнении Serbet step 2\n(при загрузке дополнительного файла)');
-        }
-    }
+    //     xhr_query_hash.onerror = function(){
+    //         alert('Ошибка соединения.\nВозникла при выполнении Serbet step 2\n(при загрузке дополнительного файла)');
+    //     }
+    // }
 
     // STEP 3 -------------------------------------
     // делаем запросы постов
