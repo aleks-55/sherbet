9582a35c32baf76f58d3df8d68b7939776623ea7
aleks-55 <aleks.rasskazov.88@yandex.ru>
2020-04-04 00:07:53 +0600

    commented out code removed

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let mymonth = '';
 let start_year = (new Date).getFullYear();
 let end_year = 2010;
 
+// Содержимое years:
 // {2020: {visible: false, domOb: div.year}, 2019: {visible: false, domOb: div.year}}
 let years = {};
 
@@ -19,7 +20,6 @@ function init() {
     document.getElementById("refresh_btn").onclick = function() {
         document.getElementById("mainBox").innerHTML = '';
         document.getElementById("description").classList.remove('visible_on');
-        // but_1.disabled = false;
         // показываем форму
         document.getElementById("mainForm").classList.remove('visible_off');
         document.getElementById("refresh_btn").classList.remove('visible_on');
@@ -40,6 +40,7 @@ function init() {
                 username = tmp_inputUser;
             }
             else {
+                console.log("Неправильно введен username или ссылка на страницу пользователя.");
                 alert("Неправильно введен username или ссылка на страницу пользователя.");
                 // чтоб не перезагружалась страница
                 return false;
@@ -48,11 +49,10 @@ function init() {
 
         // получаем номер месяца
         mymonth = document.getElementById("month_selector").value;
-        // делаем кн. неактивной
-        // this.disabled = true;
         // скрываем всю форму
         this.classList.add('visible_off');
         
+        // показываем крутящуюся шестеренку
         document.getElementById("waitingBox").classList.add('visible_on');
 
         startSherbet("https://www.instagram.com/" + username + "/");
@@ -88,7 +88,6 @@ function startSherbet(urlUserPage) {
     // берется из js-файла
     let query_hash = 'e769aa130647d2354c40ea6a439bfc08';
 
-    // STEP 1 -------------------------------------
     // получим страницу пользователя (канала)
     let xhr = new XMLHttpRequest();
     xhr.open('GET', urlUserPage);
@@ -97,6 +96,7 @@ function startSherbet(urlUserPage) {
     xhr.onload = function() {
         if (xhr.status != 200) {
             // обработать ошибку
+            console.log("Ошибка при запросе страницы пользователя.\nОтвет сервера: " + xhr.status);
             alert("Ошибка при запросе страницы пользователя.\nОтвет сервера: " + xhr.status);
             document.getElementById("refresh_btn").classList.add('visible_on');
             return;
@@ -104,19 +104,9 @@ function startSherbet(urlUserPage) {
     
         // преобразуем ответ в DOM-объект
         let doc  = new DOMParser().parseFromString(xhr.responseText, 'text/html');
-        // и получим url на js-файл в котором лежит query_hash
-        let script;
-        // script = doc.querySelectorAll("script[src]"); 
-        // let url_js = "";
-        // for (let i = 0; i < script.length; i++) {
-        //     if (script[i].src.match("ProfilePageContainer.js")) {
-        //         url_js = "https://www.instagram.com" + script[i].outerHTML.split('src="')[1].split('"')[0];
-        //         break;
-        //     }
-        // }
 
         // теперь извлечем id канала из страницы пользователя
-        script = doc.querySelectorAll("script"); 
+        let script = doc.querySelectorAll("script"); 
         for (let i = 0; i < script.length; i++) {
             if (script[i].innerHTML.match(/^window._sharedData = /)) {
                 let prefix = "window._sharedData = ";
@@ -125,58 +115,25 @@ function startSherbet(urlUserPage) {
                 break;
             }
         }
-        //return chId;
 
         // поставим title на нашу страницу-скрипт
         document.title = doc.title.split(' • ')[0] + ' • ' + document.title;
 
+        // готовим блок description
         document.getElementById('_username').innerHTML = "qwerty"//username;
         document.getElementById('countPubAll').innerHTML = getCountPubAll(xhr.responseText);
         document.getElementById('mountRusPrilog').innerHTML = getMonthRusPrilog(mymonth);
         document.getElementById("_countPub").innerHTML = pubDB.countPub;
         document.getElementById('description').classList.add('visible_on');
 
-        // step2(url_js);
-        // step 3
         request_big_count_post();
     }
 
     xhr.onerror = function(){
+        console.log('Ошибка соединения.\nВозникла при выполнении Serbet step 1\n(при загрузке страницы пользователя)');
         alert('Ошибка соединения.\nВозникла при выполнении Serbet step 1\n(при загрузке страницы пользователя)');
     }
 
-    // STEP 2 -------------------------------------
-    // загрузим js-файл и получим query_hash
-    // function step2(urlJs) {
-    //     let xhr_query_hash = new XMLHttpRequest();
-    //     xhr_query_hash.open('GET', urlJs);
-    //     xhr_query_hash.send();
-
-    //     xhr_query_hash.onload = function() {
-    //         if (xhr_query_hash.status != 200) {
-    //             // обработать ошибку
-    //             alert("Ошибка при загрузке дополнительного файла. Ответ сервера: " + xhr_query_hash.status);
-    //             document.getElementById("refresh_btn").classList.add('visible_on');
-    //             return;
-    //         }
-    //         if (t = xhr_query_hash.responseText.match(/s.pagination},queryId:"([A-Za-z0-9_-]+)"/)) {
-    //             query_hash = t[1];
-    //         } else {
-    //             alert('Ошибка.\nВ дополнительном файле не найден query_hash (queryId)');
-    //             document.getElementById("refresh_btn").classList.add('visible_on');
-    //             return;
-    //         }
-    //         //return query_hash;
-
-    //         request_big_count_post();
-    //     }
-
-    //     xhr_query_hash.onerror = function(){
-    //         alert('Ошибка соединения.\nВозникла при выполнении Serbet step 2\n(при загрузке дополнительного файла)');
-    //     }
-    // }
-
-    // STEP 3 -------------------------------------
     // делаем запросы постов
     function request_big_count_post() {
         let jsonVariables = '{"id":"' + chId + '","first":' + countPost + ',"after":"' + after + '"}';
@@ -190,6 +147,7 @@ function startSherbet(urlUserPage) {
         xhrJson.onload = function() {
             if (xhrJson.status != 200) {
                 // обработать ошибку
+                console.log("Ошибка. Ответ сервера: " + xhrJson.status);
                 alert("Ошибка. Ответ сервера: " + xhrJson.status);
                 return;
             }
@@ -217,6 +175,7 @@ function startSherbet(urlUserPage) {
         }
 
         xhrJson.onerror = function(){
+            console.log('Ошибка соединения.\nВозникла при выполнении Serbet step 3');
             alert('Ошибка соединения.\nВозникла при выполнении Serbet step 3');
         }
     }
@@ -227,7 +186,7 @@ function parsePub(pub){
     let month = d.getMonth() + 1;
     let year = d.getFullYear();
 
-    // если год публ-и не соответствует заданному году
+    // если месяц публ-и не соответствует заданному месяцу
     if (mymonth != month) { return; }
 
     let wrapper = document.createElement('div');
